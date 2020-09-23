cd904a73223d5f6a20b7e26abf0654fd17375838
Brian Napora <bnapora@gestaltdiagnostics.com>
2020-09-23 09:27:18 -0700

    clean-up changes prior to final local build test

--- score_local.py
+++ score_local.py
@@ -1,29 +1,20 @@
 import os, base64
+import glob
+import re
 import torch
 import json
+from io import BytesIO
 from azureml.core.model import Model
 from azureml.core import Workspace
 import fastai 
-# from fastai.basics import *
 from fastai.vision.all import *
 from fastai.callback.all import *
-# from fastai.metrics import accuracy 
-# from fastai.metrics import error_rate
 import urllib.request
 
-    # global model
 
-    # # The AZUREML_MODEL_DIR environment variable indicates
-    # # a directory containing the model file you registered.
-    # model_filename = 'sklearn_regression_model.pkl'
-    # model_path = os.path.join(os.environ['AZUREML_MODEL_DIR'], model_filename)
-
-    # model = joblib.load(model_path)
 
 def init():
     global learner
-    # The AZUREML_MODEL_DIR environment variable indicates a directory containing the model file you registered.  
-    #this init works 
     pathsrc=Path()
 
     model_path=os.getenv('AZUREML_MODEL_DIR')  
@@ -32,12 +23,9 @@ def init():
     classes = ['Positive','Negative']
     model_path_file = os.path.join(os.environ['AZUREML_MODEL_DIR'], filename)
     model_path_hc = 'azureml-models/breastcancerfastai/1'
-    model_path_file_hc = './source_directory/azureml-models/breastcancerfastai/1/export.pkl'
     print('AZUREML_MODEL_DIR-Model_Path: ' + model_path)
     print('Model_Path_File: ' + model_path_file)
-    print('Model_Path_File_HC: ' + model_path_file_hc)
     print('FastAI Version: ' + fastai.__version__)
-
     print ('Path(): ' + str(pathsrc))
 
     for filename in os.listdir(pathsrc):
@@ -53,19 +41,41 @@ def init():
             print(os.path.join(dirname, filename))
 
     learner = load_learner(model_path_file)
-    # classes = learner.data.classes
-    # print('Learner: ' + classes)
+    classes = learner.dls.vocab
+    print(learner.dls.vocab)
 
+def encode(img):
+    img = Image.open(BytesIO(img))
+    img = img.resize((500,500))
+    buff = BytesIO()
+    img.save(buff, format='jpeg')
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+def preprocess(image):
+    test = image
+    data = str(base64.b64encode(test), encoding='utf-8')  
+    input_data = json.dumps({'data': data})
+    return input_data
 
 def run(raw_data):
-    # base64_string = json.loads(raw_data)['data']
-    # base64_bytes = base64.b64decode(base64_string)
-    # with open(os.path.join(os.getcwd(),"score.jpg"), 'wb') as f:
-    #     f.write(base64_bytes)
-    
-    # make prediction
-    #img = open_image(os.path.join(os.getcwd(),"score.jpg"))
-    #result = learn.predict(img)
-    result = 'Test,Positive'
+    base64_string = json.loads(raw_data)['data']
+    base64_bytes = base64.b64decode(base64_string)
+
+    # img = Image.open(BytesIO(base64_bytes))
+    img = PILImage.create(BytesIO(base64_bytes))
+
+    pred_class,pred_idx,outputs = learner.predict(img)
+
+    formatted_outputs = ["{:.1f}%".format(value) for value in [x * 100 for x in torch.nn.functional.softmax(outputs, dim=0)]]
+    pred_probs = sorted(
+            zip(learner.dls.vocab, map(str, formatted_outputs)),
+            key=lambda p: p[1],
+            reverse=True
+        )
+
+    #img_data = preprocess(img)
+
+    result = {'class':pred_class, "probs":pred_probs}
+    # result = {"class":pred_class, "probs":pred_probs, "image":img_data}
     # return json.dumps({'class':str(result[0]), 'probs':result[2].data[1].item()})
-    return result
+    return json.dumps(result)
